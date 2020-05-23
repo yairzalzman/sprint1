c7c0141a900b34ade94114d63fff9e398600d149
yairzalzman <65679160+yairzalzman@users.noreply.github.com>
2020-05-23 21:45:00 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
-let bomb = '<span> &#128163 </span>'; 
+let bomb = '<span class="bombImage"> &#128163 </span>'; 
+let flag = '<span class="flagImage"> &#9872 </span>'; 
+
 let SIZE = 4;
+let timer = false
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// function gameOver() {
+//   if ()
+// }
+
 
 function buildBoard() {
     var board = [];
@@ -9,7 +21,8 @@ function buildBoard() {
         board[i][j] = returnObj();
       }
     }
-    board[0][2].isMine = true;
+    board[getRandomInt(0, 3)][getRandomInt(0, 3)].isMine = true;
+    board[getRandomInt(0, 3)][getRandomInt(0, 3)].isMine = true;
     return board;
   }
 let gboard = buildBoard();
@@ -17,7 +30,7 @@ console.log(gboard)
 
 
 function printMat(mat, selector) {
-    var strHTML = '<table class="chart" border="0"><tbody>';
+    var strHTML = ' <div class="timer" time="0">Time: 0</div> <table class="chart" border="0"><tbody>';
     for (var i = 0; i < mat.length; i++) {
       strHTML += '<tr>';
       for (var j = 0; j < mat[0].length; j++) {
@@ -26,7 +39,7 @@ function printMat(mat, selector) {
         if (cell.isMine === true) {
             className += ' bomb'
         }
-        strHTML += '<td onclick="cellClicked(this)" class="' + className + '"> '  +  bomb + '</td>'
+        strHTML += '<td row= "' + i + '"  column= "' + j + '" onclick="cellClicked(this)" oncontextmenu="plantFlag(this)" class="' + className + '"> '  +  bomb + flag + '</td>'
       }
       strHTML += '</tr>'
     }
@@ -36,11 +49,38 @@ function printMat(mat, selector) {
   }
 printMat(gboard ,'.board-container',)
 
+function plantFlag(td) {
+  td.querySelector('.flagImage').style.display = 'block'
+  if (td.classList.contains('bomb')) {
+    td.querySelector('.bombImage').style.display = 'block'
+  }
+}
+
 function cellClicked(td) {
+  if (timer === false) {
+    timer = setInterval(function() {
+      var timerElement = document.querySelector('.timer');
+      var currentTime = timerElement.getAttribute('time');
+      var nextTime = parseInt(currentTime) + 1;
+      timerElement.setAttribute('time', nextTime)
+      timerElement.innerHTML = "Time: " + nextTime
+    }, 1000);
+  }
+  let row = parseInt(td.getAttribute("row"))
+  let column = parseInt(td.getAttribute("column"))
+  let Mcount = setMinesNegsCount(gboard, row, column)
+  // document.querySelector('.minesCount').innerHTML = "Mines around: " + Mcount;
     if (td.classList.contains('bomb')){
+       let bombs = document.querySelectorAll('.bomb .bombImage' )
+       for (var i = 0; i < bombs.length; i++) {
+         bombs[i].style.display = 'block'
+       }
         td.querySelector('span').style.display = "block";
+        clearInterval(timer)
+    }    
+    else {
+      td.innerHTML = Mcount;
     }
-    console.log('bbye')
 }
 
 function returnObj() {
@@ -53,11 +93,40 @@ function returnObj() {
     };
 }
 
-function setMinesNegsCount(board) {
-    for (var i = 0; i < board.length; i++ ){
-        for(var j = 0; j < board[i].length; j++){
-           board[i][j]
-           console.log(setMinesNegsCount(board))
-        }
-    }    
+function setMinesNegsCount(board, row, column) {
+  let counterBomb = 0;
+   console.log(board[row][column])
+  //  one right
+   if (board[row][column + 1] && board[row][column + 1].isMine === true) {
+     counterBomb += 1;
+   }
+  // one left 
+   if (board[row][column - 1] && board[row][column - 1].isMine === true) {
+    counterBomb += 1;
+  }
+  //  one top
+  if (board[row - 1] && board[row - 1][column] && board[row - 1][column].isMine === true) {
+    counterBomb += 1;
+  }
+  // one bottom
+  if (board[row + 1] && board[row + 1][column] && board[row + 1][column].isMine === true) {
+    counterBomb += 1;
+  }
+  // one top one left
+  if (board[row - 1] && board[row - 1][column - 1] && board[row - 1][column - 1].isMine === true) {
+    counterBomb += 1;
+  }
+  // one bottom one left
+  if (board[row + 1] && board[row + 1][column - 1] && board[row + 1][column - 1].isMine === true) {
+    counterBomb += 1;
+  }
+  // one bottom one right
+  if (board[row - 1] && board[row - 1][column + 1] && board[row - 1][column + 1].isMine === true) {
+    counterBomb += 1;
+  }     
+  // one top one right
+  if (board[row + 1] && board[row + 1][column + 1] && board[row + 1][column + 1].isMine === true) {
+    counterBomb += 1;
+  }
+  return counterBomb;
 }
